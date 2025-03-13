2e42bcfa3dca06f011c0d4f3d8d2fc65480b6cc5
kaziabulhasib <hasibkazi420@gmail.com>
2025-03-13 21:06:09 +0530

    adding main route

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("coupon app is live");
+}); 
+
 app.use("/api/coupons", couponRoutes);
 
 const PORT = process.env.PORT || 5000;
